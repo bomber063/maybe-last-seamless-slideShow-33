a01c29d58d389cb7bdfcead184a287952a93c755
bomber <tangyihong063@163.com>
2019-05-30 13:22:45 +0800

    用${},next，length把代码变得可维护性，并写出了console.log

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(slides).css('transform', 'translateX(-400px)')
-var cloneImg2 = $('#slides > img').eq(2).clone(true)
+var intiImgeLength=$('img').length
+console.log(intiImgeLength)
+var cloneImg2 = $('#slides > img').eq(intiImgeLength-1).clone(true)
 var cloneImg0 = $('#slides > img').eq(0).clone(true)
 $('#slides').prepend(cloneImg2)
 $('#slides').append(cloneImg0)
@@ -22,6 +24,11 @@ setInterval(() => {
                 $(slides).show()
             })
         // })
+        console.log('-1200px')
+        console.log(`-${(intiImgeLength)*400}px`)
+
+        console.log('0px')
+
         console.log('0到2')
     }
     else if (current === 2 && next === 0) {
@@ -30,20 +37,30 @@ setInterval(() => {
                 $(slides).css({ 'transform': 'translateX(-400px)' }).hide().offset()//这里的offset()会返回一个包含top 和 left属性的对象，如果不写这个offset()就不能偷梁换柱。
                 $(slides).show()
             })
+        console.log('-1600px')
+        console.log(`-${(next+4)*400}px`)
+        console.log(`-${(intiImgeLength+1)*400}px`)
+
+        console.log('-400px')
+        console.log(`-${(next+1)*400}px`)
         console.log('2到0')
     }
-    else if (current === 0 && next === 1) {
+    else {
         // slides.addEventListener('click', function () {
-        $(slides).css('transform', 'translateX(-800px)')
-        console.log('0到1')
+        $(slides).css('transform', `translateX(-${(next+1)*400}px)`)
+        console.log(`-800px`)
+        console.log(`-${(next+1)*400}px`)
+        console.log('0到1 或 1到2')
         // })
     }
-    else if (current === 1 && next === 2) {
-        // slides.addEventListener('click', function () {
-        $(slides).css('transform', 'translateX(-1200px)')
-        console.log('1到2')
+    // else if (current === 1 && next === 2) {
+    //     // slides.addEventListener('click', function () {
+    //     $(slides).css('transform', 'translateX(-1200px)')
+    //     console.log(`-1200px`)
+    //     console.log(`-${(next+1)*400}px`)
+    //     console.log('1到2')
         // })
-    }
+    // }
     // }
     current = next
 }, 2000)
