76b86723df22cc38f87f4520d8f5bb50b7caecaf
bomber <tangyihong063@163.com>
2019-05-30 12:56:21 +0800

    不能增加图片，主要是current就是当前的索引，当前第几张图，current+1就是下一张图的索引，也就是下一张图片就是就几+1张。

--- main.js
+++ main.js
@@ -3,32 +3,91 @@ var cloneImg2 = $('#slides > img').eq(2).clone(true)
 var cloneImg0 = $('#slides > img').eq(0).clone(true)
 $('#slides').prepend(cloneImg2)
 $('#slides').append(cloneImg0)
-
-// $(document).ready(function () {
-    setTimeout(() => {
-        // slides.addEventListener('click',function(){
-        $(slides).css('transform', 'translateX(-800px)')
+var current = 0
+var next
+//方方老师的主要是current就是当前的索引，当前第几张图，current+1就是下一张图的索引，也就是下一张图片就是就几+1张。
+setInterval(() => {
+    next = current + 1
+    if (next > 2) {
+        next = 0
+    }
+    console.log('current是', current)
+    console.log('next是', next)
+    // if (next <= 2) {
+    if (current === 0 && next === 2) {
+        // slides.addEventListener('click', function () {
+        $(slides).css('transform', 'translateX(0px)')
+            .one('transitionend', function () {
+                $(slides).css({ 'transform': 'translateX(-1200px)' }).hide().offset()//这里的offset()会返回一个包含top 和 left属性的对象，如果不写这个offset()就不能偷梁换柱。
+                $(slides).show()
+            })
         // })
-    }, 2000)
-
-    setTimeout(() => {
-        // slides.addEventListener('click',function(){
-        $(slides).css('transform', 'translateX(-1200px)')
-        // })
-    }, 4000)
-
-    setTimeout(() => {
-        // slides.addEventListener('click',function(){
+        console.log('0到2')
+    }
+    else if (current === 2 && next === 0) {
         $(slides).css('transform', 'translateX(-1600px)')
             .one('transitionend', function () {
                 $(slides).css({ 'transform': 'translateX(-400px)' }).hide().offset()//这里的offset()会返回一个包含top 和 left属性的对象，如果不写这个offset()就不能偷梁换柱。
                 $(slides).show()
             })
+        console.log('2到0')
+    }
+    else if (current === 0 && next === 1) {
+        // slides.addEventListener('click', function () {
+        $(slides).css('transform', 'translateX(-800px)')
+        console.log('0到1')
         // })
-    }, 6000)
+    }
+    else if (current === 1 && next === 2) {
+        // slides.addEventListener('click', function () {
+        $(slides).css('transform', 'translateX(-1200px)')
+        console.log('1到2')
+        // })
+    }
+    // }
+    current = next
+}, 2000)
+
+
+// $(document).ready(function () {
+// setTimeout(() => {
+//     // slides.addEventListener('click',function(){
+//     $(slides).css('transform', 'translateX(-800px)')
+//     // })
+//     current=2
+// }, 2000)
+
+// setTimeout(() => {
+//     // slides.addEventListener('click',function(){
+//     $(slides).css('transform', 'translateX(-1200px)')
+//     // })
+//     current=3
+// }, 4000)
+
+// setTimeout(() => {
+//     // slides.addEventListener('click',function(){
+//     $(slides).css('transform', 'translateX(-1600px)')
+//         .one('transitionend', function () {
+//             $(slides).css({ 'transform': 'translateX(-400px)' }).hide().offset()//这里的offset()会返回一个包含top 和 left属性的对象，如果不写这个offset()就不能偷梁换柱。
+//             $(slides).show()
+//         })
+//     current=1
+//     // })
+// }, 6000)
 // })
 
-$('button:first').on('click',function(){
-    $(slides).css('transform', 'translateX(-400px)')
-})
-console.log($('button:first'))
+// setTimeout(() => {
+//     // slides.addEventListener('click',function(){
+//     $(slides).css('transform', 'translateX(0px)')
+//         .one('transitionend', function () {
+//             $(slides).css({ 'transform': 'translateX(-1200px)' }).hide().offset()//这里的offset()会返回一个包含top 和 left属性的对象，如果不写这个offset()就不能偷梁换柱。
+//             $(slides).show()
+//         })
+//     current = 2
+//     // })
+// }, 2000)
+// // })
+
+// $('button:first').on('click', function () {
+//     $(slides).css('transform', 'translateX(-400px)')
+// })
